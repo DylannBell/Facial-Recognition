5425577c38a86e6632137ce92ef4e92f9aba68f9
Dylan Bell <dylan-bell2@hotmail.com>
2019-05-02 15:03:56 +0800

    Added a functioning testing file and identifies all images
    Changes variable names, cleaned up code, added functionality to loop through the testing folder and correctly identifies all images in the testing file. Images in the testing file must be named in ascending order i,...,n. (i.pgm).

--- EXPERIMENTING/experimenting.m
+++ EXPERIMENTING/experimenting.m
@@ -4,93 +4,111 @@ num_training_imgs = 5;
 % Number of people (classes) in the set (3)
 num_classes = 3;
 
-% For simplicity sake just work with the first 3 directories
-dir_name = 's1';
-
 % Find number of images in folder (should be 10 - should all be the same)
-num_total_imgs = size(dir([dir_name, '\*.pgm']), 1);
+num_total_imgs = size(dir(['s1', '\*.pgm']), 1);
 
-% Testing images 
-num_testing_imgs = num_total_imgs - num_training_imgs;
+% Values for the downscaling
+c = 10;
+d = 5;
+q = c * d;
 
-% Create an empty Xi of zeros, this stores all the training data
-X = zeros(50, num_training_imgs, num_classes);
+% Create an empty training data matrix of zeros (X)
+training_data = zeros(q, num_training_imgs, num_classes);
 
-for i = 1:num_classes
-    % Keep an array of all file names and image paths (training + learning)
-    image_paths = strings(1, num_total_imgs);
-    
-    dir_name = strcat('s', num2str(i));
-    
-    % Loop through all images and save image paths into an array
-    for j = 1:num_total_imgs
-        fileName = strcat(num2str(j), '.pgm');
-        path = strcat(dir_name, '/', fileName);
-        image_paths(1, j) = path;
-    end
-    
-    % Create the Wi for the 5 training images Wi : (q * pi)
-    % What shall we call this variable? leaving it as Wi for now
-    % not sure if this is best practise to just assign an empty matrix to 0's?
-    Wi = zeros(50, num_training_imgs);
+%%% TRAINING %%%
+% Populate training data matrix
+training_data = learn_faces(training_data, num_total_imgs, num_training_imgs, num_classes, q, c, d);
 
-    for j = 1:num_training_imgs
-        % Load the image and process it
-        learning_img = imread(char(image_paths(j)));
-        learning_img = process_img(learning_img);
 
-        % Replace the ith column with the image
-        % Load the 5 training images into Wi (need to change this to 
-        % load into Xi after Wi.
-        Wi(:,j) = learning_img;   
-    end
-    
-    % Save the Wi in the Xi (which contains all the training data)
-    X(:,:,i) = Wi;
-    
+%%%% TESTING %%%
+% Load the testing directory and create image_paths
+testing_dir = uigetdir();
+num_testing_imgs = size(dir([testing_dir, '\*.pgm']), 1);
+testing_image_paths = strings(1, num_total_imgs);
+
+
+% Loop through all testing images and save image paths into an array
+for j = 1:num_testing_imgs
+    fileName = strcat(num2str(j), '.pgm');
+    path = strcat(testing_dir, '/', fileName);
+    testing_image_paths(1, j) = path;
 end
 
-% Load a testing image (6th image) and find it's hat value
-testing_img = imread(char(image_paths(6)));
-image_paths(6)
-testing_img = process_img(testing_img);
+% Load testing images
+for i = 1:num_testing_imgs
+    testing_img = imread(char(testing_image_paths(i)));
+    testing_img = process_img(testing_img, q, c, d);
 
-yhat1 = find_yhat(testing_img, X(:,:,1));
-yhat2 = find_yhat(testing_img, X(:,:,2));
-yhat3 = find_yhat(testing_img, X(:,:,3));
+    % Create a hats matrix to store all the hat values
+    hats = zeros(num_classes, 1);
 
-dist1 = norm(testing_img - yhat1)
-dist2 = norm(testing_img - yhat2)
-dist3 = norm(testing_img - yhat3)
+    for j = 1:num_classes
+        hat = find_yhat(testing_img, training_data(:,:,j));
+        dist = norm(testing_img - hat);
+        hats(j) = dist;
+    end
+
+    fprintf("Image " + i + ": ");
+    [value, index] = min(hats);
+    fprintf("This is from class " + index + "\n");
+end
+
+
+% Populate training set with face data set
+function training_data = learn_faces(training_data, num_total_imgs, num_training_imgs, num_classes, q, c, d)
+    for i = 1:num_classes
+        % Keep an array of all file names and image paths (training + learning)
+        image_paths = strings(1, num_total_imgs);
+
+        dir_name = strcat('s', num2str(i));
+
+        % Loop through all images and save image paths into an array
+        for j = 1:num_total_imgs
+            fileName = strcat(num2str(j), '.pgm');
+            path = strcat(dir_name, '/', fileName);
+            image_paths(1, j) = path;
+        end
+
+        % Create the Xi for the 5 training images Xi : (q * pi)
+        Xi = zeros(50, num_training_imgs);
+
+        for j = 1:num_training_imgs
+            % Load the image and process it
+            training_img = imread(char(image_paths(j)));
+            training_img = process_img(training_img, q, c, d);
+
+            % Replace the ith column with the image
+            % Load the 5 training images into Wi (need to change this to 
+            % load into Xi after Wi.
+            Xi(:,j) = training_img;   
+        end
+
+        % Save the Wi in the Xi (which contains all the training data)
+        training_data(:,:,i) = Xi;
+
+    end
+end
 
 % Process image
-function img = process_img(img)
-    % Values for the downscaling
-    C = 10;
-    D = 5;
-
+function img = process_img(img, q, c, d)
     % Image is already grayscale, should we check if it is or should we
     % assume all files are going to be .pgm?
     
     % Downscale image into c x d
-    img = imresize(img, [C, D]);
+    img = imresize(img, [c, d]);
     
     % Column concatenate the image so it's of size q * 1 (q = c x d)
-    q = C * D;
-    img = reshape(img, q, 1);
+    d = c * d;
+    img = reshape(img, d, 1);
     img = double(img);
     
     % Normalise the image between 0 and 1
     img = img / max(img);
 end
 
-function yhat = find_yhat(img, Wi)
-    % Algorithm to find Bhati is (XiT * Xi)^-1 * XiT * y
+function yhat = find_yhat(img, Xi)
+    % Algorithm to find bhat is (XiT * Xi)^-1 * XiT * y
     % y = img
-    % Xi is each class (Wi)
-    % Since we only one class atm : Xi = Wi
-    bhat = inv(transpose(Wi) * Wi) * transpose(Wi) * img;
-    
-    % Find yhat
-    yhat = Wi * bhat;
+    % yhat = Xi * bhat
+    yhat = Xi * ((transpose(Xi) * Xi)\transpose(Xi) * img);
 end
