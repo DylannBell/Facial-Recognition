165ba8fb032ce3208737df84241bf82b97672ee9
Dylan <dylan-bell2@hotmail.com>
2019-05-16 15:09:20 +0800

    Added popdown and made updates

--- EXPERIMENTING/GUI.m
+++ EXPERIMENTING/GUI.m
@@ -1,28 +1,13 @@
+% Introduce threshold level?
+% Unhard code constants
+% Maybe figure out how to set pop up menu to start at 5
+% Make more readable by seperating functionality into functions
+% When you close the dir without choosing a folder
+
+
+
+
 function varargout = GUI(varargin)
-% GUI MATLAB code for GUI.fig
-%      GUI, by itself, creates a new GUI or raises the existing
-%      singleton*.
-%
-%      H = GUI returns the handle to a new GUI or the handle to
-%      the existing singleton*.
-%
-%      GUI('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in GUI.M with the given input arguments.
-%
-%      GUI('Property','Value',...) creates a new GUI or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before GUI_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to GUI_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help GUI
-
-% Last Modified by GUIDE v2.5 10-May-2019 11:33:59
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -43,11 +28,13 @@ else
 end
 % End initialization code - DO NOT EDIT
 
+
 % SET CONSTANTS FOR GUI ENVIRONMENT
 % A number of constants need to be saved and readily available to functions
 % throughout this system, therefore add it as a field to handles
 % Access constants like so - handles.constants('num_training_imgs') 
 function handles = set_constants(handles, hObject)
+    
     keySet = {
                 'num_training_imgs',
                 'num_testing_imgs',
@@ -60,7 +47,19 @@ function handles = set_constants(handles, hObject)
                 'total_counter',
                 'correct_counter'
                };
-    valueSet = {5, 5, 10, 10, 10, 10, 5, 50, 0, 0};
+    valueSet = {
+                  handles.popupmenu1.Value,
+                  10-handles.popupmenu1.Value,
+                  0,
+                  0,
+                  10,
+                  10,
+                  5,
+                  50,
+                  0,
+                  0
+               };
+            
     constants = containers.Map(keySet, valueSet);
     handles.constants = constants;  
     guidata(hObject, handles);  
@@ -69,29 +68,32 @@ function handles = set_constants(handles, hObject)
 % --- Executes just before GUI is made visible.
 function GUI_OpeningFcn(hObject, eventdata, handles, varargin)
 
-% Choose default command line output for GUI
-handles.output = hObject;
+    % Choose default command line output for GUI
+    handles.output = hObject;
 
-% Update handles structure
-guidata(hObject, handles);
+    % Update handles structure
+    guidata(hObject, handles);
 
-% UIWAIT makes GUI wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
-
+    % UIWAIT makes GUI wait for user response (see UIRESUME)
+    % uiwait(handles.figure1);
 
 % --- Outputs from this function are returned to the command line.
 function varargout = GUI_OutputFcn(hObject, eventdata, handles)
-varargout{1} = handles.output;
+    varargout{1} = handles.output;
 
 
-% Process image
+% Process image according to the paper
 function img = process_img(img, handles)
+
+    % Load constants from handles
     q = handles.constants('q');
     c = handles.constants('c');
     d = handles.constants('d');
     
-    % Image is already grayscale, should we check if it is or should we
-    % assume all files are going to be .pgm?
+    % Convert to grayscale if needed
+    if ndims(img) == 3
+        img = rgb2gray(img);
+    end
     
     % Downscale image into c x d
     img = imresize(img, [c, d]);
@@ -104,14 +106,19 @@ function img = process_img(img, handles)
     % Normalise the image between 0 and 1
     img = img / max(img);
 
-
+ 
 % Populate training set with face data set
 function training_data = learn_faces(handles)
 
-    training_data = zeros(handles.constants('q'), handles.constants('num_training_imgs'), handles.constants('num_training_classes'));
+    % Load constants from handles
     num_training_classes = handles.constants('num_training_classes');
     num_training_imgs = handles.constants('num_training_imgs');
+    q = handles.constants('q');
     
+    % Initialise the training data matrix (X)
+    training_data = zeros(q, num_training_imgs, num_training_classes);
+    
+    % Load 
     training_dir = dir("Training");
     
     for i = 1:num_training_classes
@@ -122,8 +129,9 @@ function training_data = learn_faces(handles)
         
         % Loop through all images and save image paths into an array
         for j = 1:num_training_imgs
-            fileName = strcat(num2str(j), '.pgm');
-            path = strcat(dir_name, '/', fileName);
+            current_dir = dir("Training/" + dir_name);
+            fileName = current_dir(j+2).name;
+            path = strcat("Training/", dir_name, "/", fileName);
             image_paths(1, j) = path;
         end
 
@@ -131,16 +139,16 @@ function training_data = learn_faces(handles)
         class_layer = zeros(50, num_training_imgs);
         
         % For each image in the training images class
-        for j = 1:num_training_imgs
+        for k = 1:num_training_imgs
             
             % Load the image and process it
-            training_img = imread(char(image_paths(j)));           
+            training_img = imread(char(image_paths(k)));           
             training_img = process_img(training_img, handles);
             
             % Replace the ith column with the image
             % Load the 5 training images into Wi (need to change this to 
             % load into Xi after Wi.
-            class_layer(:,j) = training_img;   
+            class_layer(:,k) = training_img;   
         end
 
         % Save the Wi in the Xi (which contains all the training data)
@@ -157,31 +165,36 @@ function yhat = find_yhat(img, Xi)
 
 function test_faces(handles, testing_dir_path, training_data)
 
+    % Load constands from handles
     num_testing_imgs = handles.constants('num_testing_imgs');
+    num_training_imgs = handles.constants('num_training_imgs');
     num_total_imgs = handles.constants('num_total_imgs');
     num_testing_classes = handles.constants('num_testing_classes');
     num_training_classes = handles.constants('num_training_classes');
     
+    % Load Training and Testing directories
     training_dir = dir("Training");
     testing_dir = dir(testing_dir_path);
     
     for i = 1:num_testing_classes
         
         % Keep an array of all file names and image paths for training data
-        testing_image_paths = strings(1, num_testing_imgs);
+
+        testing_image_paths = strings(1, num_total_imgs);
         
         
         class_testing_dir = testing_dir(i+2).name;
-
+        
         % Loop through all images and save image paths into an array
         for j = 1:num_total_imgs
-            fileName = strcat(num2str(j), '.pgm');
+            current_dir = dir(strcat(testing_dir_path, '/', class_testing_dir));
+            fileName = current_dir(j+2).name;
             path = strcat(testing_dir_path, '/', class_testing_dir, '/', fileName);
             testing_image_paths(1, j) = path;
         end
         
         % Load testing images
-        for k = num_testing_imgs+1:num_total_imgs
+        for k = num_training_imgs+1:num_total_imgs
             testing_img = imread(char(testing_image_paths(k)));
             
             % Display testing image on LHS
@@ -199,16 +212,19 @@ function test_faces(handles, testing_dir_path, training_data)
                 hats(m) = dist;
             end
             
+            % PUT THIS BELOW INTO A NEW FUNCTION
+            
             % Obtain where current photo is from
             current_class_int = str2num(class_testing_dir(2));
             [min_value, min_index] = min(hats);
-            fprintf("Class: " + current_class_int + ", Image: " + k);
-            fprintf("Index = " + min_index + "\n");
+            %fprintf("Class: " + current_class_int + ", Image: " + k);
+            %fprintf("Index = " + min_index + "\n");
                         
             % Display Correct Image
-            %correct_img_path = "Training/" + strcat('s', num2str(min_index)) + '/1.pgm';
-            correct_img_path = "Training/" + training_dir(min_index+2).name + "/1.pgm"
-            correct_img = imread(correct_img_path);
+            correct_img_path = "Training/" + training_dir(min_index+2).name;
+            correct_img_dir = dir(correct_img_path);
+            correct_img_name = correct_img_dir(3).name;
+            correct_img = imread(strcat(correct_img_path, '/', correct_img_name));
             imshow(correct_img, 'Parent', handles.axes2);
             
             % Display Y-Hat Value
@@ -220,7 +236,7 @@ function test_faces(handles, testing_dir_path, training_data)
             
             total_counter = total_counter + 1;
             
-            if training_dir(min_index+2).name == class_testing_dir
+            if strcmp(training_dir(min_index+2).name, class_testing_dir)
                 correct_counter = correct_counter + 1;
             end
             
@@ -238,14 +254,19 @@ function test_faces(handles, testing_dir_path, training_data)
 % automatically traverse some expected directory stucture for training and
 % testing images
 function pushbutton1_Callback(hObject, eventdata, handles)
-    % initialise environment
+
+    % Initialise constants
     handles = set_constants(handles, hObject);
 
     % Open folder selection dialog box and save testing path
     % Catch Error if no directory is chosen
     testing_dir = uigetdir();
     
+    
     %%% TRAINING %%%%
+    
+    % Load the training path and all subfolders into matlab
+    addpath(genpath("Training"));
     
     % Determine number of classes by counting the directories
     training_dir = dir("Training");
@@ -254,7 +275,7 @@ function pushbutton1_Callback(hObject, eventdata, handles)
     
     % This finds number of total images in first dir (assume all are the
     % same)
-    handles.constants('num_total_imgs') = size(dir(['Training\s1', '\*.pgm']), 1);
+    handles.constants('num_total_imgs') = size(dir(['Training\s1', '\']), 1)-2;
     
     
     % Initialise + Populate training data matrix
@@ -268,15 +289,18 @@ function pushbutton1_Callback(hObject, eventdata, handles)
     handles.constants('num_testing_classes') = num_testing_classes;
     
     % Test data
-    test_faces(handles, testing_dir, training_data)
+    test_faces(handles, testing_dir, training_data);
     
 
     
-    
-
-
 % --- Executes during object creation, after setting all properties.
 function text7_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to text7 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+    if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+        set(hObject,'BackgroundColor','white');
+    end
